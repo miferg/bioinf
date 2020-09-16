44a7cc742213ba44ab3115a5a486c860750e71b5
Miguel F. Romero <43483471+miferg@users.noreply.github.com>
2020-09-16 17:30:13 -0500

    Add files via upload

--- esearch/esrch.py
+++ esearch/esrch.py
@@ -6,7 +6,7 @@ handle.close()
 edge_list = []
 
 # load all files and make a list of bbh pairs of tuples (edges)
-# each touple has the database and peptide
+# each touple has the database and sequence
 for filename in files:
     handle = open(filename,'r')
     bbh_list = handle.read().split('\n')[:-1]
@@ -22,14 +22,24 @@ print(edge_list)
 i = 0
 while i < len(edge_list):
     seed = edge_list[0]
+    print('seed:')
+    print(seed)
     edge_list = edge_list[1:]
     output = [seed]
     ii = 0
     while ii < len(edge_list):
         subject = edge_list[ii]
-        if seed[0] in subject or seed[1] in subject:
-            output.append(edge_list.pop(ii))
-            ii += -1
+        print('subject:')
+        print(subject)
+        for vertex in seed:
+            if vertex in subject:
+                print('success')
+                output.append(edge_list.pop(ii))
+                seed = seed+subject
+                seed = list(set(seed))
+                ii += -1
+                break
         ii += 1
     i += 1
+    print('output:')
     print(output)
