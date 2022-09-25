dba85982edc9cc5d595f0c77e269d1ef05d9f12b
Miguel F. Romero <43483471+miferg@users.noreply.github.com>
2022-09-25 15:36:40 -0700

    Update m5nr_annotation.py
    added COG and improved readability

--- m5nr/m5nr_annotation.py
+++ m5nr/m5nr_annotation.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Get the annotations from a list of md5s.
+Get annotations from a list of md5s.
 """
 
 import pickle
 import sys
 import os
 
-db_dict = {
-        'm5nr_genbank.dict':['GenBank','97'],
-        'm5nr_refseq.dict':['RefSeq','60'],
-        'm5nr_seed.dict':['SEED','20'],
-        'm5nr_kegg.dict':['KEGG','6']
-        }
-
-if len(sys.argv[1:]) == 0:
+try:
+    db_file = sys.argv[1]
+    m5_file = sys.argv[2]
+    outprefix = sys.argv[3]
+    
+except:
     print("""
                 Get the annotations from a list of md5s.
-                usage: python3 m5nr_annotation.py database.dict md5s.txt output
+                usage: python3 m5nr_annotation.py database.dict md5s.txt output_prefix
                 Available databases: 
                 m5nr_genbank.dict = GenBank
                 m5nr_refseq.dict = RefSeq
                 m5nr_seed.dict = SEED
                 m5nr_kegg.dict = KEGG GENES
+                m5nr_cog.dict = COG
                 Miguel F. Romero github.com/miferg
                 """
                 )
@@ -31,76 +30,168 @@ if len(sys.argv[1:]) == 0:
 
 argv = sys.argv[1:]
 
+### DEFINITIONS
 
+db_dict = {
+        'm5nr_genbank.dict':['GenBank','97'],
+        'm5nr_refseq.dict':['RefSeq','60'],
+        'm5nr_seed.dict':['SEED','20'],
+        'm5nr_kegg.dict':['KEGG','6'],
+        'm5nr_cog.dict':['COG','0.005']
+        }
 
-print()
-print('''
-Get the annotations from a list of md5s.
-Miguel F. Romero github.com/miferg
-''')
+def greet():
+'''
+Say hi
+'''
+    print()
+    print('''
+    Get the annotations from a list of md5s.
+    Miguel F. Romero github.com/miferg
+    ''')
+    return 0
 
-dbfile = os.path.abspath(argv[0]).split('/')[-1]
+def check_db(dbfile, dbdict):
+'''
+Check if the database file is supported
+'''
+    if dbfile in db_dict.keys():
+        print('Database: '+ db_dict[dbfile][0])
+        print('Loading database, this may take a few minutes (and ~'+ db_dict[dbfile][1] +' GBs of memory).')
+        return 0
+    
+    else:
+        print('Please use one of the available databases.\n')
+        sys.exit()
+        
+def genbank(m5_file, database, outfile, absent):
+'''
+Write the genbank annotations
+'''
+    with open(m5_file, 'r') as infile:
+        for line in infile:
+            md5 = line.strip()
+            try:
+                outfile.write(md5 +'\t'+ database[md5] +'\n')
+            except:
+                absent.append(md5)
+    return outfile, absent
 
-if dbfile in db_dict.keys():
-    print('Database: '+ db_dict[dbfile][0])
-else:
-    print('Please use one of the available databases.\n')
-    sys.exit()
+def refseq(m5_file, database, outfile, absent):
+'''
+Write the refseq annotations
+'''
+    with open(m5_file, 'r') as infile:
+        for line in infile:
+            md5 = line.strip()
+            try:
+                outfile.write(md5 +'\t'+ '\t'.join(list(database[md5])) +'\n')
+            except:
+                absent.append(md5)
+    return outfile, absent
+        
+def seed(m5_file, database, outfile, absent):
+'''
+Write the seed annotations
+'''
+    with open(m5_file, 'r') as infile:
+        for line in infile:
+            md5 = line.strip()
+            try:
+                ssids = database[md5]
+                for line in ssids:
+                    outfile.write(md5 +'\t'+ line)
+            except:
+                absent.append(md5)
+    return outfile, absent
 
-print('Loading database, this may take a few minutes (and ~'+ db_dict[dbfile][1] +' GBs of memory).')
+def kegg(m5_file, database, outfile, absent):
+'''
+Write the kegg annotations
+'''
+    with open(m5_file, 'r') as infile:
+        for line in infile:
+            md5 = line.strip()
+            try:
+                outfile.write(md5 +'\t'+ database[md5] +'\n')
+            except:
+                absent.append(md5)
+    return outfile, absent
 
-database = pickle.load(open(os.path.abspath(argv[0]), "rb"))
+def cog(m5_file, database, outfile, absent):
+'''
+Write the cog annotations
+'''
+    with open(m5_file, 'r') as infile:
+        for line in infile:
+            md5 = line.strip()
+            try:
+                outfile.write(md5 +'\t'+ database[md5]) +'\n')
+            except:
+                absent.append(md)
+    return outfile, absent
+    
+def get_table(m5_file, database, dbfile, outfile, absent):
+'''
+Set the database to use and run the dedicated function
+''' 
+    if dbfile == 'm5nr_genbank.dict':
+        return genbank(m5_file, database, outfile, absent)
+                
+    if dbfile == 'm5nr_refseq.dict':
+        return refseq(m5_file, database, outfile, absent)
+                
+    if dbfile == 'm5nr_seed.dict':
+        return seed(m5_file, database, outfile, absent)
+                
+    if dbfile == 'm5nr_kegg.dict':
+        return kegg(m5_file, database, outfile, absent)
+                
+    if dbfile == 'm5nr_cog.dict':
+        return cog(m5_file, database, outfile, absent)
 
-print('Loading input.')
+def check_absents(absent):
+'''
+Write the absent md5s if any
+'''
+    if len(absent) > 0:
+        print('There were '+ str(len(absent)) +' md5s missing in the annotation database.')
+        print('Check the '+ outprefix+'.'+db_dict[dbfile][0]+'. missing.txt file.')
+        with open(outprefix+'.'+db_dict[dbfile][0]+'. missing.txt','w') as outfile:
+            outfile.write('\n'.join(absent) +'\n')
+    return 0
+    
+### MAIN PROGRAM
 
-handle = open(argv[1],'r')
-md5s = handle.read().split('\n')[:-1]
-handle.close()
+def main():
+    
+    greet()
 
-print('Writing output in '+ argv[2])
-outfile = open(argv[2], 'w')
+    dbfile = os.path.abspath(db_file).split('/')[-1]
 
-absent = []
+    check_db(dbfile, dbdict)
 
-if dbfile == 'm5nr_genbank.dict':
-    for md5 in md5s:
-        try:
-            outfile.write(md5 +'\t'+ database[md5] +'\n')
-        except:
-            absent.append(md5)
+    with open(os.path.abspath(db_file), "rb") as infile:
+        database = pickle.load(infile)
+
+    print('Writing output in '+ outprefix+'.'+db_dict[dbfile][0]+'.tsv')
+    
+    outfile = open(outprefix+'.'+db_dict[dbfile][0]+'.tsv', 'w')
+
+    absent = []
+    
+    outfile, absent = get_table(m5_file, database, dbfile, outfile, absent)
+
     outfile.close()
 
-elif dbfile == 'm5nr_refseq.dict':
-    for md5 in md5s:
-        try:
-            outfile.write(md5 +'\t'+ '\t'.join(list(database[md5])) +'\n')
-        except:
-            absent.append(md5)
-    outfile.close()
+    check_absents(absent)
 
-elif dbfile == 'm5nr_seed.dict':
-    for md5 in md5s:
-        try:
-            ssids = database[md5]
-            for line in ssids:
-                outfile.write(md5 +'\t'+ line)
-        except:
-            absent.append(md5)
-    outfile.close()
+    print('Done!\n')
+    
+    return 0
 
-elif dbfile == 'm5nr_kegg.dict':
-    for md5 in md5s:
-        try:
-            outfile.write(md5 +'\t'+ database[md5] +'\n')
-        except:
-            absent.append(md5)
-    outfile.close()
-
-if len(absent) > 0:
-    print('There were '+ str(len(absent)) +' md5s missing in the annotation database.')
-    print('Check the m5nr_annotation_missing.txt file.')
-    outfile = open('m5nr_annotation_missing.txt','w')
-    outfile.write('\n'.join(absent) +'\n')
-    outfile.close()
-
-print('Done!\n')
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit("\n(Terminating due to user interrupt signal)")
